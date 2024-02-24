5c12a03b5b4e5558e4f75b55978f86293999928f
Juan Pepe <juancitopepe965@gmail.com>
2024-02-24 06:09:51 +0000

    Done creating all test for source and position_abc_bank model and its yml file

--- models/refined/position_abc_bank.sql
+++ models/refined/position_abc_bank.sql
@@ -2,4 +2,4 @@ select
     *
     , position_value - cost_base as unrealized_profit
     , round(unrealized_profit / cost_base, 5) as unrealized_profit_pct
-from {{ source('abc_bank', 'ABC_BANK_POSITION') }}
+from {{ source('abc_bank', 'abc_bank_position') }}
